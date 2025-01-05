fd900c2418f24dc14230fd0ed28242bdb322f5b7
jefta-jose <ndegwajeff4@gmail.com>
2025-01-05 04:47:42 +0300

    added drop down functionality for services link

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -2,117 +2,10 @@
 
 import Image from "next/image";
 import { motion } from "framer-motion";
+import { Services } from "@/utils/servicesArray";
 
 
-const page = () => {
-  const Services = [
-    {
-      id: 1,
-      name: 'Commercial Interior Design',
-      description: 'Transform commercial spaces with designs that boost brand image and foster employee collaboration.',
-      image: '/image/project3.jpg',
-      link: '',
-    },
-    {
-      id: 2,
-      name: 'Residential Interior Design',
-      description: 'Design personalized homes that reflect your style, enhance comfort, and create lasting memories.',
-      image: '/image/project2.jpg',
-      link: '',
-    },
-    {
-      id: 3,
-      name: 'Gypsum Ceilings',
-      description: 'Add elegance and functionality to your home with beautiful gypsum ceiling designs.',
-      image: '/image/project3.jpg',
-      link: '',
-    },
-    {
-      id: 4,
-      name: 'Modern Office Design',
-      description: 'Create a functional and modern office space that promotes productivity and innovation.',
-      image: '/image/project3.jpg',
-      link: '',
-    },
-    {
-      id: 5,
-      name: 'Luxury Living Room Design',
-      description: 'Design luxurious living rooms that provide comfort and timeless style.',
-      image: '/image/project2.jpg',
-      link: '',
-    },
-    {
-      id: 6,
-      name: 'Customized Bedroom Interiors',
-      description: 'Design tailored bedrooms that blend aesthetics with functionality.',
-      image: '/image/project3.jpg',
-      link: '',
-    },
-    {
-      id: 7,
-      name: 'Kitchen Renovations',
-      description: 'Innovate your kitchen with practical and stylish designs that meet all your culinary needs.',
-      image: '/image/project3.jpg',
-      link: '',
-    },
-    {
-      id: 8,
-      name: 'Outdoor Living Spaces',
-      description: 'Enhance outdoor areas with designs perfect for relaxation and entertainment.',
-      image: '/image/project2.jpg',
-      link: '',
-    },
-    {
-      id: 9,
-      name: 'Eco-Friendly Interiors',
-      description: 'Incorporate sustainable and eco-friendly designs into your interior spaces.',
-      image: '/image/project3.jpg',
-      link: '',
-    },
-    {
-      id: 10,
-      name: 'Creative Kids Room Design',
-      description: 'Design playful and inspiring rooms for children to grow and thrive.',
-      image: '/image/project3.jpg',
-      link: '',
-    },
-    {
-      id: 11,
-      name: 'Elegant Bathroom Designs',
-      description: 'Upgrade bathrooms with elegant, modern, and functional designs.',
-      image: '/image/project2.jpg',
-      link: '',
-    },
-    {
-      id: 12,
-      name: 'Home Office Interiors',
-      description: 'Design home offices that inspire productivity and comfort.',
-      image: '/image/project3.jpg',
-      link: '',
-    },
-    {
-      id: 13,
-      name: 'Retail Space Design',
-      description: 'Create retail spaces that captivate customers and boost sales.',
-      image: '/image/project3.jpg',
-      link: '',
-    },
-    {
-      id: 14,
-      name: 'Event Space Interiors',
-      description: 'Design event spaces that are stylish, functional, and memorable.',
-      image: '/image/project2.jpg',
-      link: '',
-    },
-    {
-      id: 15,
-      name: 'Hospitality Design',
-      description: 'Elevate guest experiences with interior designs for hotels, restaurants, and lounges.',
-      image: '/image/project3.jpg',
-      link: '',
-    },
-  ];
-  
+const Servicespage = () => {
 
   return (
           <div className="container grid lg:grid-cols-3 gap-8 py-8 mt-4">
@@ -120,7 +13,7 @@ const page = () => {
               Services.map((service) => (
                 <motion.div
                 key={service.id}
-                className="relative"
+                className="relative dark:bg-tertiary"
                 whileHover={{
                   y: -10, // Moves up on hover
                   x: -10, // Moves left on hover
@@ -144,4 +37,4 @@ const page = () => {
   )
 }
 
-export default page
+export default Servicespage
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,8 +1,14 @@
+'use client'
+
+import { useState } from "react";  // Import useState hook
+
 import Link from "next/link";
 
 //Hooks
 import { usePathname } from "next/navigation";
 import { motion } from "framer-motion";
+
+import { Services } from "@/utils/servicesArray";
 
 const links = [
     { path: '/', name: 'Home' },
@@ -15,35 +21,91 @@ const links = [
     { path: '/contact', name: 'Contact Us' },
 ]
 
-const Nav = ({ containerStyles, linkStyles, underlineStyles , onLinkClick }) => {
 
+
+const Nav = ({ containerStyles, linkStyles, underlineStyles , onLinkClick }) => {
+    const [isHovered, setIsHovered] = useState(false);  // State to track hover
+    const [dropdownVisible, setDropdownVisible] = useState(false); // State to show/hide dropdown
     const path = usePathname();
+  
+    const handleMouseEnter = () => {
+      setIsHovered(true);
+      setDropdownVisible(true);  // Show dropdown on hover
+    };
+  
+    const handleMouseLeave = () => {
+      setIsHovered(false);
+      setDropdownVisible(false);  // Hide dropdown when hover ends
+    };
+
+    const handleClickService = () => {
+      setDropdownVisible(false);  // Hide dropdown when a service is clicked
+    };
+
     return (
         <nav className={`${containerStyles}`}>
-            {
-                links.map((link, index) => {
-                    return (
-                        <Link
-                        href={link.path}
-                        key={index}
-                        className={`uppercase ${linkStyles}`}
-                        onClick={onLinkClick}
-                        >
-                            {link.path === path && (
-                                <motion.span
-                                initial={{ y: '-100%' }}
-                                animate={{ y: 0 }}
-                                transition={{ type: 'tween' }}
-                                layoutId='underline'
-                                className={`${underlineStyles}`} 
-                                />
-                            )}
-                        {link.name}
-                        </Link>
-                    )
-                })
-            }
-        </nav>
+        {links.map((link, index) => {
+          if (link.name === "Services") {
+            return (
+              <div key={index} className="relative"
+                onMouseEnter={handleMouseEnter}  // Attach hover events to parent div
+                onMouseLeave={handleMouseLeave}
+              >
+                <Link
+                  href={link.path}
+                  className={`uppercase ${linkStyles}`}
+                  onClick={onLinkClick}
+                >
+                  {link.name}
+                </Link>
+  
+                {/* Dropdown */}
+                {dropdownVisible && isHovered && (
+                  <div
+                    className="absolute left-0 top-4 mt-2 w-60 bg-white dark:bg-gray-800 rounded-lg shadow-lg p-4 z-10"
+                    onMouseEnter={() => setDropdownVisible(true)}  // Keep dropdown visible when mouse enters it
+                    onMouseLeave={() => setDropdownVisible(false)}  // Hide dropdown when mouse leaves it
+                  >
+                    <ul>
+                      {Services.map((service) => (
+                        <li key={service.id}>
+                          <Link
+                            href="#"
+                            className="block py-2 px-4 text-gray-700 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-600"
+                            onClick={handleClickService}
+                          >
+                            {service.name}
+                          </Link>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                )}
+              </div>
+            );
+          }
+  
+          return (
+            <Link
+              href={link.path}
+              key={index}
+              className={`uppercase ${linkStyles}`}
+              onClick={onLinkClick}
+            >
+              {link.path === path && (
+                <motion.span
+                  initial={{ y: "-100%" }}
+                  animate={{ y: 0 }}
+                  transition={{ type: "tween" }}
+                  layoutId="underline"
+                  className={`${underlineStyles}`}
+                />
+              )}
+              {link.name}
+            </Link>
+          );
+        })}
+      </nav>
     )
 }
 
